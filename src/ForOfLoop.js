150b442ff2ff94a65745500ec5cb6d59bda42134
SHARIER ISLAM <Sharierpulok475@gmail.com>
2025-04-23 12:08:46 +0800

    Basic All  loops

---
+++ src/ForOfLoop.js
@@ -0,0 +1,10 @@
+// for-of Loop use for string & Array
+
+let str ="Sharier";
+let size = 0;
+for (let val of str){
+    console.log("Val =", val);
+    size++;
+}
+
+console.log("String Size =",size);
