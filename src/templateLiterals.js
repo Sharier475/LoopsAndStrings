dc5be3da6cf1003615ea12db019cfb6d209a3eea
SHARIER ISLAM <Sharierpulok475@gmail.com>
2025-04-28 23:35:41 +0800

    Initial Practice Done

--- src/templateLiterals.js
+++ src/templateLiterals.js
@@ -6,3 +6,8 @@ let obj = {
 let outPut = `The cost of the ${obj.item} is ${obj.price} Taka only.`
 
 console.log(outPut);
+console.log(`Sharier ${2+3}`);
+console.log(`Sharier \n Islam.`); //Escape Character to next line.
+console.log(`Sharier \t Islam.`); //a single Tab Space.
+
+
